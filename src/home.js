986573437e493f191788c83078dea497b6204fe3
Piyush Barve <69067476+piyushbarve98@users.noreply.github.com>
2020-10-24 00:41:04 +0530

    axios added for fetch functionality

--- src/home.js
+++ src/home.js
@@ -1,13 +1,50 @@
-import React from "react";
+import React,{Component} from "react";
+import axios from 'axios';
 
+class Home extends Component{
 
-function Home(){
-  return(
-    <div className='Container mt-4'>
-    <h1 className='title'>HomePage</h1>
-    <p className='subtitle'>You are at HomePage</p>
+  state ={
+    posts: []
+  }
+
+  componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/posts').then((res)=>{
+      this.setState({
+          posts : res.data.slice(0,10)
+      }
+      )
+    })
+  }
+  render(){
+    const {posts} = this.state;
+    const postsList = posts.length ? (posts.map((post)=>{
+      return(
+      <div className='post card mt-5'>
+        <div className='card-header'>
+          <div className='card-header-title is-centered'>
+            {post.title}
+          </div>
+        </div>
+        <div className='card-content'>
+        
+        <div className='content'>{post.body}</div>
+        </div>
+        
+
+        
+      </div>
+      )
+    })):(
+    
+        <p>Nothing to show now</p>
+    
+    )
+    return(
+    <div className='Container mx-4 my-4'>
+      {postsList}
     </div>
   )
+  }
 }
 
 export default Home
